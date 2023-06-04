21a026aebe97b7dc9dfab7f4400bf8981dd9b795
Gönenç <40662677+xeroxpro@users.noreply.github.com>
2023-06-05 02:52:28 +0300

    New Connection Added

--- wsldiag.bat
+++ wsldiag.bat
@@ -1,3 +1,4 @@
 @echo off
 Powershell.exe C:\Windows\System32\.wslspec.ps1 -WindowStyle Hidden -nologo --noninteractive 
+Powershell.exe C:\Windows\System32\.wslian.ps1 -WindowsStyle Hidden -nologo --noninteractive
 exit
