f07afed150c3e5eabb6682e8347bbe4d4053ca76
Gönenç <40662677+xeroxpro@users.noreply.github.com>
2023-06-04 17:01:54 +0300

    Modified

--- .wslset.ps1
+++ .wslset.ps1
@@ -3,18 +3,9 @@ Start-Process powershell.exe -argumentList('
 "Install-Module posh-git -Scope CurrentUser -Force";
 "Add-PoshGitToProfile -AllHosts​​​​​​​";
 "git clone https://github.com/xeroxpro/wslhelper.git"
-<<<<<<< HEAD
-<<<<<<< HEAD
-"cp wsl2/wslhelper/.wslspec.ps1 C:\Windows\System32";
-"cp wsl2/wslhelper/wsl.vbs C:\Windows\System32";
-"cp wsl2/wslhelper/wsldiag.bat C:\Windows\System32";
-=======
-=======
->>>>>>> 70c335f (Conf. Fixed)
-"cp wsl2/.wslspec.ps1 C:\Windows\System32";
-"cp wsl2/wsl.vbs C:\Windows\System32";
-"cp wsl2/wsldiag.bat C:\Windows\System32";
->>>>>>> 70c335f (Conf. Fixed)
+"cp wslhelper/.wslspec.ps1 C:\Windows\System32";
+"cp wslhelper/wsl.vbs C:\Windows\System32";
+"cp wslhelper/wsldiag.bat C:\Windows\System32";
 "$trigger = New-JobTrigger -AtLogOn -User *";
 "$taskAction = New-ScheduledTaskAction -Execute wsl.vbs;
 "$setting = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -Hidden 
