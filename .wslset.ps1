824a86e5a16a13e6a6b0f331775c028bb22cccbe
Gönenç <40662677+xeroxpro@users.noreply.github.com>
2023-06-04 15:32:12 +0300

    Conf. Fixed

--- .wslset.ps1
+++ .wslset.ps1
@@ -3,9 +3,15 @@ Start-Process powershell.exe -argumentList('
 "Install-Module posh-git -Scope CurrentUser -Force";
 "Add-PoshGitToProfile -AllHosts​​​​​​​";
 "git clone https://github.com/xeroxpro/wslhelper.git"
+<<<<<<< HEAD
 "cp wsl2/wslhelper/.wslspec.ps1 C:\Windows\System32";
 "cp wsl2/wslhelper/wsl.vbs C:\Windows\System32";
 "cp wsl2/wslhelper/wsldiag.bat C:\Windows\System32";
+=======
+"cp wsl2/.wslspec.ps1 C:\Windows\System32";
+"cp wsl2/wsl.vbs C:\Windows\System32";
+"cp wsl2/wsldiag.bat C:\Windows\System32";
+>>>>>>> 70c335f (Conf. Fixed)
 "$trigger = New-JobTrigger -AtLogOn -User *";
 "$taskAction = New-ScheduledTaskAction -Execute wsl.vbs;
 "$setting = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -Hidden 
