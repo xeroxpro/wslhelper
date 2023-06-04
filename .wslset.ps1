4b18e5eb85667301bbc8bb12941d91d62df98e21
PureDevOps <40662677+xeroxpro@users.noreply.github.com>
2023-06-04 15:22:51 +0300

    Update .wslset.ps1

--- .wslset.ps1
+++ .wslset.ps1
@@ -2,10 +2,10 @@ Start-Process powershell.exe -argumentList('
 "Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned -Force";
 "Install-Module posh-git -Scope CurrentUser -Force";
 "Add-PoshGitToProfile -AllHosts​​​​​​​";
-"git clone https://github.com/xeroxpro/wsl2.git"
-"cp wsl2/.wslspec.ps1 C:\Windows\System32";
-"cp wsl2/wsl.vbs C:\Windows\System32";
-"cp wsl2/wsldiag.bat C:\Windows\System32";
+"git clone https://github.com/xeroxpro/wslhelper.git"
+"cp wsl2/wslhelper/.wslspec.ps1 C:\Windows\System32";
+"cp wsl2/wslhelper/wsl.vbs C:\Windows\System32";
+"cp wsl2/wslhelper/wsldiag.bat C:\Windows\System32";
 "$trigger = New-JobTrigger -AtLogOn -User *";
 "$taskAction = New-ScheduledTaskAction -Execute wsl.vbs;
 "$setting = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -Hidden 
