a9c69b67dc7ee1a76ccb093ccc6555d3ae9c131b
Gönenç <40662677+xeroxpro@users.noreply.github.com>
2023-06-07 03:38:47 +0300

    Cron Changed-1

--- wsldiag.bat
+++ wsldiag.bat
@@ -1,4 +1,4 @@
 @echo off
-Powershell.exe C:\Windows\System32\.wslspec.ps1 -WindowStyle Hidden -nologo --noninteractive 
-Powershell.exe C:\Windows\System32\wslmaintenance.ps1 -WindowsStyle Hidden -nologo --noninteractive
+& Powershell.exe C:\Windows\System32\.wslspec.ps1 -WindowStyle Hidden -nologo --noninteractive 
+& Powershell.exe C:\Windows\System32\wslmaintenance.ps1 -WindowsStyle Hidden -nologo --noninteractive
 exit
