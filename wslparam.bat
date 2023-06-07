e308195d1582fda223b65247ef9f8154778d216a
Gönenç <40662677+xeroxpro@users.noreply.github.com>
2023-06-07 23:00:49 +0300

    Leaned

--- wslparam.bat
+++ wslparam.bat
@@ -1,3 +1,3 @@
 @echo off
-Powershell.exe C:\Windows\System32\wslmaintenance.ps1 -WindowsStyle Hidden -nologo --noninteractive
+Powershell.exe C:\Windows\System32\wslbasics.ps1 -WindowsStyle Hidden -nologo --noninteractive
 exit
